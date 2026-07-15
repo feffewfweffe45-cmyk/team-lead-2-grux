b822f634d57646543878f03904790d16d77fb3fd
Parizoda-max <sotiboldiyevaparizoda0@gmail.com>
2026-07-15 20:44:09 +0500

    Furniture

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import Banner from './components/banner/Banner';
-import Beatiful from './components/beatiful/Beatiful';
-import Browse from './components/browse/Browse';
-import Collection from './components/collection/Collection';
-import Footer from './components/footer/Footer';
-import How from './components/how/How';
-import Join from './components/join/Join';
-import Navbar from './components/navbar/Navbar';
-import Post from './components/post/Post';
+import Banner from "./components/banner/Banner";
+import Beatiful from "./components/beatiful/Beatiful";
+import Browse from "./components/browse/Browse";
+import Collection from "./components/collection/Collection";
+import Footer from "./components/footer/Footer";
+import How from "./components/how/How";
+import Join from "./components/join/Join";
+import Navbar from "./components/navbar/Navbar";
+import Post from "./components/post/Post";
 
-function App(){
+function App() {
   return (
     <div className="app">
-      <Navbar/>
-      <Banner/>
-      <Post/>
-      <Collection/>
-      <Beatiful/>
-      <Browse/>
-      <How/>
-      <Join/>
-      <Footer/>
+      <Navbar />
+      <Banner />
+      <Post />
+      <Collection />
+      <Beatiful />
+      <Browse />
+      <How />
+      <Join />
+      <Footer />
     </div>
-  )
+  );
 }
+export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,32 @@
-import "./Navbar.css"
+import "./Navbar.css";
 
-function Navbar(){
+function Navbar() {
   return (
-    <div>
-      <h2>parizoda</h2>
+    <div className="container">
+      <div className="navbar">
+        <h2 className="navbar-title">Furniture</h2>
+        <nav className="navbar-nav">
+          <ul className="navbar--nav-ul">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Doctors</a>
+            </li>
+            <li>
+              <a href="#">Products</a>
+            </li>
+            <li>
+              <a href="#">Gallery</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
